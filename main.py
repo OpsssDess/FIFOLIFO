de11aa0fdad99941ede808d96611bf68de8e1570
OpsssDess <OpsssDess@icloud.com>
2022-05-25 15:20:51 +0300

    переделал с двух складов на один

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # LIFO and FIFO
 
 
-stack = {"LIFO": [], "FIFO": []}
+stack = []
 
 while True:
     print('Вы хотите взять или положить?(put or take)')
@@ -12,29 +12,21 @@ while True:
         answer = input('')
         thing = input('Что отдаёте? ')
         if answer == 'LIFO':
-            stack["LIFO"].append(thing)
+            stack.append(thing)
             print(stack)
         else:
-            stack["FIFO"].insert(0, thing)
+            stack.insert(0, thing)
             print(stack)
 
     else:
-        if bool(stack["FIFO"]) is False and bool(stack["LIFO"]) is False:
+        if not stack:
             print('сейчас можно только положить!')
             continue
         print('Откуда Вам выдать вещь (LIFO или FIFO)?')
         answer = input('')
         if answer == 'LIFO':
-            if bool(stack["LIFO"]) is False:
-                print('LIFO пуст')
-                continue
-            else:
-                print(f'возьмите {stack["LIFO"].pop()}')
+                print(f'возьмите {stack.pop()}')
                 print(stack)
         else:
-            if bool(stack["FIFO"]) is False:
-                print('FIFO пуст')
-                continue
-            else:
-                print(f'возьмите {stack["FIFO"].pop()}')
+                print(f'возьмите {stack.pop()}')
                 print(stack)
